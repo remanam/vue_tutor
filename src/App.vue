2edee8cb7a5fd5e41e61e461f630a89918f363c8
remanam <47778545+remanam@users.noreply.github.com>
2023-04-30 18:34:34 +0300

    31:37

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
+    <div class="app">
+        <form @submit.prevent>
+        <h4>Создание поста</h4>
+        <input 
+            v-bind:value="title"
+            @input="title = $event.target.value" 
+            class="input" 
+            type="text" 
+            placeholder="Название">
+        <input 
+            v-bind:value="body" 
+            @input="body = $event.target.value" 
+            class="input" 
+            type="text" 
+            placeholder="Описание">
+        <button class="btn" @click="createPost">Создать пост</button>
+        </form>
+    </div>
+
     <div class="post" v-for="post in posts" v-bind:key="post.id">
-        <div><strong>Название: </strong> {{ post.tittle }} </div>
+        <div><strong>Название: </strong> {{ post.title }} </div>
         <div><strong>Описание: </strong> {{ post.body }} </div>
     </div>
 </template>
@@ -13,8 +32,23 @@ export default {
                 {id: 1, tittle: 'Javascript', body: "Описание поста"},
                 {id: 2, tittle: 'Javascript 2', body: "Описание поста 2"},
                 {id: 3, tittle: 'Javascript 3', body: "Описание поста 3"},
-            ]
+            ],
+            title: '',
+            body: '',
+
         }
+    },
+    methods: {
+        createPost() {
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body,
+            }
+            this.posts.push(newPost)
+            this.title = '';
+            this.body = '';
+        },
     },
 }
 
@@ -27,10 +61,35 @@ export default {
     box-sizing: border-box;
 }
 
+.app {
+    padding: 20px;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+}
+
 .post {
     padding: 15px;
     border: 2px solid teal;
     margin-top: 15px;
+}
+
+.input {
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+}
+
+.btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
 }
 
 </style>
