4b17366339e88db0fcc2150567df5b71c43056a3
remanam <47778545+remanam@users.noreply.github.com>
2023-04-30 12:17:57 +0300

    commite 22:55 on video https://www.youtube.com/watch?v=XzLuMtDelGk&t=499s

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="post" v-for="post in posts" v-bind:key="post.id">
+        <div><strong>Название: </strong> {{ post.tittle }} </div>
+        <div><strong>Описание: </strong> {{ post.body }} </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+            posts: [
+                {id: 1, tittle: 'Javascript', body: "Описание поста"},
+                {id: 2, tittle: 'Javascript 2', body: "Описание поста 2"},
+                {id: 3, tittle: 'Javascript 3', body: "Описание поста 3"},
+            ]
+        }
+    },
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
+
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+
 </style>
