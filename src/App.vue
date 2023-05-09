01e01cbd969ecdeaf5433f068ec0631d9e3a0758
ErikRem <remanam@mail.ru>
2023-05-09 18:58:42 +0300

    1:10:00

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
     <div class="app">
         <post-form @create="createPost"/>
         <post-list      
-            :posts="posts"/>       
+            :posts="posts"
+            @remove="removePost"/>       
     </div>
 
 </template>
@@ -32,6 +33,9 @@ export default {
         createPost(post) {
             this.posts.push(post)
         },
+        removePost(post){
+            this.posts = this.posts.filter(p => p.id !== post.id)
+        }
     },
 }
 
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,15 @@
 <template>
-    <div>
-        <h3>Список пользователей</h3>
+    <div v-if="posts.length > 0 ">
+        <h3>Список постов</h3>
         <post-item         
             v-for="post in posts"
             :key="post.id"
-            :post="post"   
+            :post="post"
+            @remove="$emit('remove', post)"  
             />
     </div>
+    <h2 v-else style="color: red">
+            Список постов пуст</h2>
 </template>
 
 <script>
